8d081fd5c1b3570f729ddfe507aebd30b305f1c4
Steffany Paiva <76757826+Steffany7406@users.noreply.github.com>
2024-09-26 21:06:24 -0300

    Update question1.js

--- question1.js
+++ question1.js
@@ -1,3 +1,8 @@
+/*Elabore um programa em Javascript que leia a velocidade permitida em uma estrada e a velocidade de um condutor. 
+Se a velocidade for inferior ou igual à permitida, exiba “Sem Multa”. Se a velocidade for de até 20% maior que a permitida, exiba “Multa Leve”. 
+E, se a velocidade for superior a 20% da velocidade permitida, exiba “Multa Grave”.  Considerar os valores/tempos da tabela a seguir:*/
+
+
 let vel = 100; //velocidade
 let cond = 65;
 
