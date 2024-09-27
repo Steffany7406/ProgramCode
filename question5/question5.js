4e2ea46e8b9a7d36ad9c26dd4991a3d736cbb03d
Steffany Paiva <76757826+Steffany7406@users.noreply.github.com>
2024-09-26 21:43:11 -0300

    Add files via upload

---
+++ question5/question5.js
@@ -0,0 +1,24 @@
+var n1 = prompt("Digite o número:")
+var n2 = prompt("Digite o próximo:")
+
+var calculadora = {
+    somar: function somarNumeros(n1, n2) {
+       //
+    },
+
+    subtrair: function subtrairNumeros(n1, n2) {
+       //
+    },
+    
+    multiplicar: function multiplicarNumeros(n1, n2) {
+        //
+    },
+
+    dividir: function dividirNumeros(n1, n2) {
+        //
+    }
+
+}
+alert(somarNumeros());
+alert(subtrairNumeros());
+alert(dividirNumeros());
