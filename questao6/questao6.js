eafafc0c511c2f21462e9570717722ab221b7ae5
Steffany Paiva <76757826+Steffany7406@users.noreply.github.com>
2024-09-26 21:44:57 -0300

    Add files via upload

---
+++ questao6/questao6.js
@@ -0,0 +1,18 @@
+var array = [5, 6, 87, 29, 42, 13];
+
+var ab = prompt("Digite um número:").value;
+var cd = prompt("Agora mais um!").value;
+
+let incluse = [array.push(ab,cd)];
+incluse.sort();
+
+console.log(incluse);
+
+
+/*function incluse(){
+
+array.push() + ab;
+array.push() + cd;
+array.reverse();
+
+}alert(incluse(array.length));*/
