46f1b08a6a716f6a49b28fd74e88a8b73723592b
Steffany Paiva <76757826+Steffany7406@users.noreply.github.com>
2024-09-27 21:23:19 -0300

    Update and rename question2.js to NumerosRepetidos.js

---
+++ NumerosRepetidos.js
@@ -0,0 +1,9 @@
+/*
+Elaborar um programa que adicione números a um vetor. 
+O programa deve impedir a inclusão de números repetidos. 
+Exibir a lista de números a cada inclusão.
+*/
+
+const vetor = [120, 80, 100, 240, 37, 45];
+vetor.push(90, 25);
+console.log(vetor);
--- question2.js
+++
@@ -1,3 +0,0 @@
-const vetor = [120, 80, 100, 240, 37, 45];
-vetor.push(90, 25);
-console.log(vetor);
